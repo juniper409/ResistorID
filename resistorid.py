5a83419332c16aabf92f6553455f18aa298f8dc3
Juniper409 <mrjuniper409@gmail.com>
2019-01-29 22:42:28 -0500

    Adding wording

---
+++ resistorid.py
@@ -0,0 +1,22 @@
+print("Welcome to Resistor ID!")
+
+
+print("Locate the colored rings on the resistor. The end with three bands will be entered first from the outside inward")
+
+
+
+
+
+print("Locate the outside ring. This ring is the outlier from the other three")
+
+# Variables
+blue_dig = 6
+black_dig = 0
+brown_dig = 1
+red_dig =2
+orange_dig = 3
+yellow_dig = 4
+green_dig = 5
+violet_dig = 7
+
+
